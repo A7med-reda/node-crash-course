6a105b03b49c52274cb0a37c32700e9ef4756aea
Ahmed Reda <69392524+A7med-reda@users.noreply.github.com>
2022-10-07 15:19:33 +0200

    Update streams.js

--- streams.js
+++ streams.js
@@ -1,8 +1,13 @@
-const fs = require('fs');
+//streams object allow to you to (read / write) using data before it has finished loading .
+// data deliverd in chunck of data , package in buffer ,it move to the user when buffer fill in chunck 
+// we using streams when we deal with larg and big files
 
+const fs = require('fs');
+//there is 4 types of streams
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
-
+every type of streams is eventemitter (send event)
+//streams have 4 event (data , end ,error ,finish)
 readStream.on('data', chunk => {
   // console.log('---- NEW CHUNK ----');
   // console.log(chunk);

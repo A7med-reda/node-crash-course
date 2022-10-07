c84c0495c747b7991ae20efa4b56c7b8b9c8bd23
Ahmed Reda <69392524+A7med-reda@users.noreply.github.com>
2022-10-07 15:23:36 +0200

    Update streams.js

--- streams.js
+++ streams.js
@@ -3,11 +3,15 @@
 // we using streams when we deal with larg and big files
 
 const fs = require('fs');
-//there is 4 types of streams
+
+//there is 4 types of streams( deplux ,Transform)
+
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
-every type of streams is eventemitter (send event)
+
+//every type of streams is eventemitter (send event)
 //streams have 4 event (data , end ,error ,finish)
+
 readStream.on('data', chunk => {
   // console.log('---- NEW CHUNK ----');
   // console.log(chunk);

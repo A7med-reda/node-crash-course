02ba0a42b525576b7dcc87893fcc8706b51e8ea6
Ahmed Reda <69392524+A7med-reda@users.noreply.github.com>
2022-12-06 15:46:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,20 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
+  /*
+  res.send()
+  set header content-type html (default)
+  set statues-code  =200   (default)
+  end the res .end( )       (default)
+ */
   // res.send('<p>home page</p>');
+  
+  /*
+  we must tell Express that(./views...) is relative to root path(project that filles in app.js) or where he from
+  so we use option obj {root: } to set it as reltive root path of our project =__dirname .
+  or we can use Path Module .
+  if we didnt do that => Express will go to look for the default absoule root path of computer.
+   */
   res.sendFile('./views/index.html', { root: __dirname });
 });
 
@@ -22,6 +35,12 @@ app.get('/about-us', (req, res) => {
 });
 
 // 404 page
+/*
+  Use()-> is fire to every single (all) request coming and fire middleware if req didnt match any previous routs
+  [.get() ,.post ,.delete()].
+   so  the postion must be in bottom of routes to handle 404 
+  used to create middleware fun()and fire them.
+*/
 app.use((req, res) => {
   res.status(404).sendFile('./views/404.html', { root: __dirname });
 });

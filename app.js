89ffae2fe4008838ee304b06398ae6a56b892f07
Ahmed Reda <69392524+A7med-reda@users.noreply.github.com>
2022-12-11 13:19:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ app.listen(3000);
 // register view engine
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
-
+/*
+   ejs  view || template engine 
+ - allow us to write HTML template very semilir to HTML index
+ - inject dynmic data & logic into them like Variables,loops 
+ - then serve /send them to client 
+ - it render template from Views dirctory. we can edit app.set('views', )..
+*/
 app.get('/', (req, res) => {
   const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},

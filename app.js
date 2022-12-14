fa222d0d992a84bdb5fcec1330505f8bfeabe4ab
Ahmed Reda <69392524+A7med-reda@users.noreply.github.com>
2022-12-15 01:22:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 // express app
 const app = express();
 /*
- middleware don't run in sequntial(express think you senf res) so the next middleware(any code) will not run.
+ middleware don't run in sequntial(express think you send res) so the next middleware(any code) will not run.
  unless you fire next() 
 */
 
@@ -16,6 +16,12 @@ app.listen(3000);
 // register view engine
 app.set('view engine', 'ejs');
 
+/*
+express.static(root)
+a built-in middleware function in Express. 
+It serves static files(js css image)to browser(front-end)
+because server protect your static file from accsse from browser 
+*/
 // middleware & static files
 app.use(express.static('public'));
 
@@ -31,7 +37,11 @@ app.use((req, res, next) => {
   console.log('in the next middleware');
   next();
 });
-
+/*
+Morgan library is an HTTP request logger middleware 
+Logs the HTTP requests along with some other information. 
+ dev - tiny format
+*/
 app.use(morgan('dev'));
 
 app.use((req, res, next) => {
